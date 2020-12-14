864fddc530f5570e69cc271cf80d52c9129ac7e7
Muhammed Salman Shamsi <muhammed-salman@users.noreply.github.com>
2020-12-14 15:51:03 +0530

    Dev tool initial commit

---
+++ lib/devcampmss_view_tool.rb
@@ -0,0 +1,7 @@
+require "devcampmss_view_tool/version"
+require "devcampmss_view_tool/renderer"
+
+module DevcampmssViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/devcampmss_view_tool/version.rb
@@ -0,0 +1,3 @@
+module DevcampmssViewTool
+  VERSION = "0.1.0"
+end
